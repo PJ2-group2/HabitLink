99c936d934e2768db6f6680c04533e4f9b5cbbbf
Serina Sakurai <mickeysnow1031@gmail.com>
2025-07-08 14:55:22 +0900

    Refactor TeamTopController and Config to streamline imports
    enhance HTTP handling, logging, and debug support

--- src/main/java/com/habit/client/util/Config.java
+++ src/main/java/com/habit/client/util/Config.java
@@ -3,27 +3,42 @@ package com.habit.client.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * アプリケーションの設定を管理するクラス。
  * プロパティファイルからサーバアドレスを読み込み、クライアントに提供する。
- * 
+ *
  */
 public class Config {
-    private static final Properties properties = new Properties();
+  private static final Logger logger = LoggerFactory.getLogger(Config.class);
 
-    static {
-        try (InputStream input = Config.class.getClassLoader().getResourceAsStream("application.properties")) {
-            if (input == null) {
-                System.out.println("Sorry, unable to find application.properties");
-            }
-            properties.load(input);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+  private static final Properties properties = new Properties();
 
-    public static String getServerUrl() {
-        return properties.getProperty("server.url");
+  static {
+    try (InputStream input = Config.class.getClassLoader().getResourceAsStream(
+             "application.properties")) {
+      if (input == null) {
+        logger.error("Sorry, unable to find application.properties");
+      } else {
+        properties.load(input);
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
+  }
+
+  public static String getServerUrl() {
+    String server_url = (String)properties.getProperty("server.url");
+    logger.debug("server url: {}", server_url);
+    return server_url;
+  }
+
+  public static boolean getIsDebug() {
+    String debug_key = (String)properties.getProperty("debug");
+    debug_key = debug_key.trim();
+    logger.debug("debug key: {}", debug_key);
+    return debug_key.equals("1");
+  }
 }
